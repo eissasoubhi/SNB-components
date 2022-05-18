f479c0e6589e828c6de740e3c5c00d9f5c0104bd
Eissa Soubhi <eissa.soubhi@gmail.com>
2022-05-18 14:33:28 +0200

    build 1.1.4

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     output: {
         path: path.resolve( __dirname, 'dist' ),
         filename: 'snb-components.min.js',
+        libraryTarget: "commonjs2"
     },
 
     // file resolutions
