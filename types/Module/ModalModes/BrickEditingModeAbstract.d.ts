2abcce0591426a064c5f1ef1ab221e801f2d8962
Aissa SOUBHI <asoubhi@nexity.fr>
2023-01-06 11:44:22 +0100

    Build

--- types/Module/ModalModes/BrickEditingModeAbstract.d.ts
+++ types/Module/ModalModes/BrickEditingModeAbstract.d.ts
@@ -2,10 +2,11 @@ import Editor from "../../Editor";
 import ModalModeAbstract from "./ModalModeAbstract";
 import ModalModeInterface from '../Interfaces/Modal/ModalModeInterface';
 import DataInterface from "../Interfaces/DataInterface";
+import ModalOptionsInterface from "../Interfaces/Modal/ModalOptionsInterface";
 export default abstract class BrickEditingModeAbstract extends ModalModeAbstract implements ModalModeInterface {
     private readonly editingBrick;
     constructor(editingBrick: HTMLElement, editor: Editor);
     save(data: DataInterface): void;
-    getModalLoadData(): DataInterface;
+    getModalLoadData(modalOptions: ModalOptionsInterface): DataInterface;
 }
 //# sourceMappingURL=BrickEditingModeAbstract.d.ts.map
